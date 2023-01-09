22b9f3ae464b44e0d824afa08f2050c35887a54e
AminS <safarin207@gmail.com>
2023-01-09 18:51:37 +0330

    Refactor exercise #03

--- Q03/main.js
+++ Q03/main.js
@@ -2,12 +2,12 @@ const parentTextDiv = document.getElementById("parentText");
 const targetTextDiv = document.getElementById("targetText");
 const childTextDiv = document.getElementById("childNode");
 
-const copyParentButton = document.getElementById("copyParent");
-const copyChildButton = document.getElementById("copyChild");
-
 const parentTextCopy = parentTextDiv.innerHTML;
 const targetTextCopy = targetTextDiv.innerHTML;
 const childTextCopy = childTextDiv.innerHTML;
+
+const copyParentButton = document.getElementById("copyParent");
+const copyChildButton = document.getElementById("copyChild");
 
 copyParentButton.addEventListener("click", function () {
   targetTextDiv.innerHTML =
